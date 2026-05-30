6f3d14ecbcfa525430031d0323a7e792fa970eb8
Sebastian Kubacki <seb.kubacki@gmail.com>
2026-05-30 17:26:57 +0200

    New feat: parse ATM logs into structured device event maps

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env/python 
 f = open("../logs/atm.log")
-device = {}
-i = 0
-TID = []
-timeStamp = []
-status = []
-information = []
-for line in f:
-		TID.append([line.split("|")[0]])
-		timeStamp.append([line.split("|")[1]])
-		status.append([line.split("|")[2]])
-		information.append([line.split("|")[3]])
 
-print(TID)
-print(timeStamp)
-print(status)
-print(information)
+parts = []
+
+#PARSOWANIE DANYCH
+
+for line in f:
+	# rodzielamy po "|"
+	# usuwamy spacje bo "/ \n"
+	parsed_line = [x.strip() for x in line.split("|")]
+	
+	#pomijamy puste linie
+	if len(parsed_line) > 1:
+		parts.append(parsed_line)
+
+#TWORZENIE SŁOWNIKA
+
+devices = {}
+
+for part in parts:
+	device_id = part[0]
+	
+	event = part[1:]
+	
+	if device_id not in devices:
+		devices[device_id] = []
+
+	devices[device_id].append(event)
+
+#PRINT
+
+for device in devices:
+	print(device)
+	print(devices[device])
+	print()
+		
+
+	
