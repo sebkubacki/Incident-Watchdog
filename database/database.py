174586ed8afd3851f62246a9fd749415a5a8c5ff
Sebastian Kubacki <seb.kubacki@gmail.com>
2026-06-09 07:29:21 +0200

    feeat: implement incident insertfaion into SQLite
    Create first INSERT operations for incidents table.
    Learn parameterized SQL queries using placeholders.
    Add incident insertion workflow and verify records directly in SQLite.

--- database/database.py
+++ database/database.py
@@ -17,7 +17,35 @@ create_table = """
 def create_tables(con):
     con.execute(create_table)
 
+
+device = 'ATM001'
+timestamp = '2026-06-09 06:49:55'
+status = 'ERROR'
+module = 'NETWORK'
+event = 'CONNECTION LOST'
+
+values = (
+        device,
+        timestamp,
+        status,
+        module,
+        event
+    )   
+
+sql = """
+    INSERT INTO incidents (
+        device_id,
+        timestamp,
+        status,
+        module,
+        event
+    ) VALUES (?, ?, ?, ?, ?)
+"""
+
+
+
 con = create_connection()
-create_tables(con)
+#create_tables(con)
+con.execute(sql, values)
 con.commit()
 print("END")
